84b3a54834e56a1f5e53f7d8a1f4ab770fb2deac
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2018-06-06 13:36:18 -0300

    Add ESLint and fix js files

--- cli.js
+++ cli.js
@@ -1,99 +1,99 @@
 import RocketChat from './rocketchat';
-var inquirer = require("inquirer");
+const inquirer = require('inquirer');
 
-let clients = [];
+const clients = [];
 
 const connect = (a) => {
 	for (let index = 0; index < a; index++) {
 		const client = new RocketChat(null);
 		client.oncePing = function() {
 			this.registerUser();
-		}
+		};
 		clients.push(client);
 	}
 	console.log('connected');
 
 	return true;
-}
-
-const writeMessage = async () => {
-  const answers = await inquirer.prompt([
-    {
-      type: "input",
-	  name: "message",
-	  default: 'hi rocketchat',
-      message: "write your message"
-    }
-  ]);
-  return answers.message;
 };
 
-const message = async (n) => {
+const writeMessage = async() => {
+	const answers = await inquirer.prompt([
+		{
+			type: 'input',
+			name: 'message',
+			default: 'hi rocketchat',
+			message: 'write your message'
+		}
+	]);
+	return answers.message;
+};
+
+const message = async(n) => {
 	const m = await writeMessage();
 	for (let index = 0; index < index < n; index++) {
 		const element = clients[index % clients.length];
 		element.sendMessage('GENERAL', m);
 	}
-}
-const howMany = async () => {
-	const answers = await inquirer.prompt([
-    {
-      type: "input",
-      name: "number",
-      message: "how many?",
-      validate: function(value) {
-        var pass = value.match(
-          /^[0-9]+]?$/i
-        );
-        if (pass) {
-          return true;
-        }
-        return "Please enter a valid number";
-      }
-    }
-  ]);
-  return answers.number
-
-}
-const users = async () => {
+};
+const howMany = async() => {
 	const answers = await inquirer.prompt([
 		{
-			type: "list",
-			name: "action",
-			message: "What do you want?",
-			choices: ["Connect", "Disconnect", "Message"],
-			filter: function(val) {
-			return val.toLowerCase();
+			type: 'input',
+			name: 'number',
+			message: 'how many?',
+			validate(value) {
+				const pass = value.match(
+					/^[0-9]+]?$/i
+				);
+				if (pass) {
+					return true;
+				}
+				return 'Please enter a valid number';
+			}
 		}
+	]);
+	return answers.number;
+
+};
+const users = async() => {
+	const answers = await inquirer.prompt([
+		{
+			type: 'list',
+			name: 'action',
+			message: 'What do you want?',
+			choices: ['Connect', 'Disconnect', 'Message'],
+			filter(val) {
+				return val.toLowerCase();
+			}
 		}
 	]);
 	let n;
 	switch (answers.action) {
-		case "connect":
+		case 'connect':
 			n = await howMany();
 			return connect(n);
-		case "disconnect":
+		case 'disconnect':
 			n = await howMany();
 			return connect(n);
-		case "message":
+		case 'message':
 			n = await howMany();
 			await message(n);
-  }
-}
-(async () => {
-  while (true) {
-    const answers = await inquirer.prompt([{
-		type: "list",
-		name: "action",
-		message: "What do you want?",
-		choices: ["Users", "Status"],
-		filter: function(val) {
-			return val.toLowerCase();
+	}
+};
+(async() => {
+	while (true) { // eslint-disable-line no-constant-condition
+		const answers = await inquirer.prompt([{
+			type: 'list',
+			name: 'action',
+			message: 'What do you want?',
+			choices: ['Users', 'Status'],
+			filter(val) {
+				return val.toLowerCase();
+			}
+		}]);
+		switch (answers.action) {
+			case 'users':
+				await users();
 		}
-	  }]);
-	  switch(answers.action) {
-		  case 'users':
-			await users()
-	  }
-  }
+	}
 })();
